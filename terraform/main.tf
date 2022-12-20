7d3fca0fb17c3c6dadf5fd5a1e302e389c3b609d
Ingridy Martins <ingridy.martins@ze.delivery>
2022-12-20 15:31:08 -0300

    Alteração de arquivos.

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,12 @@ resource "aws_s3_bucket" "data-bucket-name" {
 resource "aws_s3_bucket" "etl-bucket-name" {
   bucket = var.etl-bucket-name
 }
+
+data "terraform_remote_state" "my_state" {
+  backend = "s3"
+  config = {
+    bucket = aws_s3_bucket.etl-bucket-name.id
+    key    = "tfstate/tfstate.tfstate"
+    region = "us-east-1"
+  }
+}
