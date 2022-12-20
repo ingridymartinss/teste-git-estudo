404605ef8dea128d345e7bcb6bcd83b54905ba5f
Ingridy Martins <ingridy.martins@ze.delivery>
2022-12-20 15:53:18 -0300

    Removendo tfstate.

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,25 +4,13 @@ resource "aws_s3_bucket" "data-bucket-name" {
 
 resource "aws_s3_bucket" "etl-bucket-name" {
   bucket = var.etl-bucket-name
-  policy = <<-EOF
-  {
-      "Version": "2012-10-17",
-      "Statement": [
-          {
-              "Effect": "Allow",
-              "Action": "s3:*",
-              "Resource": "*"
-          }
-      ]
-  }
-  EOF
 }
 
-data "terraform_remote_state" "my_state" {
-  backend = "s3"
-  config = {
-    bucket = aws_s3_bucket.etl-bucket-name.id
-    key    = "tfstate/tfstate.tfstate"
-    region = "us-east-1"
-  }
-}
+# data "terraform_remote_state" "my_state" {
+#   backend = "s3"
+#   config = {
+#     bucket = aws_s3_bucket.etl-bucket-name.id
+#     key    = "tfstate/tfstate.tfstate"
+#     region = "us-east-1"
+#   }
+# }
