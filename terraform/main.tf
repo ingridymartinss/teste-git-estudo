b06e552cfc2b361aec4fd8f1a28d49603c7d725d
Ingridy Martins <ingridy.martins@ze.delivery>
2022-12-20 15:50:59 -0300

    Alteração.

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,15 +4,6 @@ resource "aws_s3_bucket" "data-bucket-name" {
 
 resource "aws_s3_bucket" "etl-bucket-name" {
   bucket = var.etl-bucket-name
-}
-
-data "terraform_remote_state" "my_state" {
-  backend = "s3"
-  config = {
-    bucket = aws_s3_bucket.etl-bucket-name.id
-    key    = "tfstate/tfstate.tfstate"
-    region = "us-east-1"
-  }
   policy = <<-EOF
   {
       "Version": "2012-10-17",
@@ -25,4 +16,13 @@ data "terraform_remote_state" "my_state" {
       ]
   }
   EOF
+}
+
+data "terraform_remote_state" "my_state" {
+  backend = "s3"
+  config = {
+    bucket = aws_s3_bucket.etl-bucket-name.id
+    key    = "tfstate/tfstate.tfstate"
+    region = "us-east-1"
+  }
 }
