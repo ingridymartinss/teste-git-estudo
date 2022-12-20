9a5e6a0343b16de907ba41285c0633ad5ddcd344
Ingridy Martins <ingridy.martins@ze.delivery>
2022-12-20 16:43:41 -0300

    Alteração de tf.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,17 @@
 resource "aws_s3_bucket" "data-bucket-name" {
   bucket = var.data-bucket-name
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "etl-bucket-name" {
   bucket = var.etl-bucket-name
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tfstateingridy"
+    key    = "tfstate"
+    region = "us-east-1"
+  }
 }
