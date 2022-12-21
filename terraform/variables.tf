d7ff817a977174a7dcc752392f157b9bd6a0b4ff
Ingridy Martins <ingridy.martins@ze.delivery>
2022-12-20 21:28:51 -0300

    teste.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,8 +7,3 @@ variable "etl-bucket-name" {
   type        = string
   description = "ETL Bucket Name"
 }
-
-variable "code-sync-bucket-name" {
-  type        = string
-  description = "Code Sync Bucket Name"
-}
--- terraform/vars.tfvars
+++ terraform/vars.tfvars
@@ -1,3 +1,2 @@
 data-bucket-name = "ingridy-data"
 etl-bucket-name = "ingridy-data-etl"
-code-sync-bucket-name = "s3://ingridy-data-etl/code"
