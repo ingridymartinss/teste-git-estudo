a660c958067ca5cedd31585803ef11f84e628a18
Ingridy Martins <ingridy.martins@ze.delivery>
2022-12-21 19:31:49 -0300

    teste

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "data-bucket-name"{
     type = string
-    description = "Data Bucket Name 2"
+    description = "Data Bucket Name"
 }
 
 variable "etl-bucket-name" {
   type        = string
-  description = "Data Bucket Name 2"
+  description = "Data Bucket Name"
 }
